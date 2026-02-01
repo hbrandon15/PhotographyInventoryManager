ada2527a9982d30393c6b1af51cfce853cbfe84d
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2026-01-31 18:24:43 -0600

    Add JSON dependency. Add new equipment

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var equipment = new Equipment()
+        {
+            Type = "Laptop",
+            Name = "Dell XPS 13",
+            Manufacturer = "Dell",
+            Description = "A high-end laptop for professionals.",
+            Quantity = 10,
+            Location = "Warehouse A",
+            ID = 1
+        };
+
+        string jsonString = JsonSerializer.Serialize(equipment);
+        Console.WriteLine(jsonString);
+
+    }
+}
