d216fe0a0d74df993f8cd031dd69723feafdd86f
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2026-02-02 11:04:18 -0600

    Added methods to create camera

--- Camera.cs
+++ Camera.cs
@@ -6,4 +6,14 @@
         Console.WriteLine("Camera created.");
 
     }
+
+    public void CreateCamera()
+    {
+        createEquipment(); // Call base class method to create common equipment properties
+        Console.WriteLine("Camera created with specific properties.");
+    }
+    public void ViewCamera()
+    {
+        viewEquipment(); // Call base class method to view common equipment properties
+    }
 }
