49b345deceac9244d4e5413143bb258e74362a5e
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2026-02-02 11:19:47 -0600

    Created a list to store and display new equipment

--- Program.cs
+++ Program.cs
@@ -4,9 +4,20 @@
     {
         Console.WriteLine("Hello, World!");
 
-        var equipment1 = new Equipment();
-        equipment1.createEquipment();
-        equipment1.viewEquipment();
+        //var equipment1 = new Equipment();
+        //equipment1.createEquipment();
+        //equipment1.viewEquipment();
+        List<Equipment> equipmentList = new List<Equipment>();
+        var camera1 = new Camera();
+        camera1.CreateCamera();
+        equipmentList.Add(camera1);
+
+        foreach (var equipment in equipmentList)
+        {
+            equipment.viewEquipment();
+        }
+
+
 
         //string jsonString = JsonSerializer.Serialize(equipment);
         //Console.WriteLine(jsonString);
