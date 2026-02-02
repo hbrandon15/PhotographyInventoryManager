77674ad5a3ed4498d2b80814b7acef8a2655efa5
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2026-02-02 08:32:09 -0600

    Add lens properties

--- Lens.cs
+++ Lens.cs
@@ -1,5 +1,9 @@
 ﻿public class Lens : Equipment
 {
+    public string FocalLength { get; set; }
+    public string LowestAperture { get; set; }
+    public string MountType { get; set; }
+
     public Lens() // Constructor
     {
         Console.WriteLine("Lens created.");
